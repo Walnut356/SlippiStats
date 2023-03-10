2eb25c069f33d22e73b75c8db3e45a9c9b949d9f
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-03-09 22:53:30 -0600

    cleaning

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    author="melkor",
-    author_email="hohav@fastmail.com",
+    author="Walnut",
+    author_email="walnut356@gmail.com",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    description="Parsing library for SSBM replay files",
+    description="Stats library for SSBM replay files",
     install_requires=['py-ubjson', 'termcolor'],
     long_description=long_description,
     long_description_content_type="text/x-rst",
     name="py-slippi",
     packages=setuptools.find_packages(),
-    python_requires='~=3.7',
+    python_requires='~=3.10',
     tests_require=['mypy', 'types-termcolor'],
-    url="https://github.com/hohav/py-slippi",
-    version="1.6.2",
+    url="https://github.com/Walnut356/py-slippi-stats",
+    version="0.1.0",
 )
