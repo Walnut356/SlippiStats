c701f13573dbb6726f7f078efb187ba6a9255e63
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-04-11 07:10:07 +0000

    refactored get_player and get_opponent

--- slippistats/stats/computer.py
+++ slippistats/stats/computer.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from itertools import permutations
 from os import PathLike
 
+from numpy import False_
+
 
 from ..enums.character import CSSCharacter
 from ..event import Frame, Start
@@ -10,19 +12,27 @@ from ..game import Game
 from ..util import Base, Ports
 from .stat_types import Data
 
-
+ValueError
 class IdentifierError(Exception):
+    """Connect code or port identifier does not match any players in the available Game object."""
     pass
 
 
 class PlayerCountError(Exception):
+    """The Game object's player count is not 2"""
+    pass
+
+class PlayerTypeError(Exception):
+    """One or both players is not Player.Type.HUMAN"""
     pass
 
 
 @dataclass
 class Player(Base):
-    """Aggregate class for event.Start.Player and metadata.Player.
-    Also contains stats info"""
+    """Aggregates info from event.Start.Player and metadata.Player. Also contains all frames for the player's port.
+
+    Stats and Combo output is stored here.
+    """
 
     character: CSSCharacter
     port: Ports
@@ -71,6 +81,33 @@ class Player(Base):
 
 # TODO abstract base class?
 class ComputerBase:
+    """Base for Computer classes, used for processing parsed replays.
+
+    Currently only accepts replays with 2 human players
+
+    Attributes:
+        replay : Game
+            The current parsed replay object
+        replay_version : Start.SlippiVersion
+            The parsed replay's version number. Can be compared to tuples and strings - e.g. (0, 1, 0), "0.1.0"
+        replay_path : Pathlike | str
+            Filepath used to parse the current replay if one was provided. Required for dolphin queue export
+        players : list[Player]
+            Contains metadata and all frames for each player in the game. Generated stats and combos are stored here
+        queue : list[dict]
+            formatted list of events used to create Clippi/Dolphin compatible json queues for playback
+
+    Methods:
+        prime_replay -> None
+            Takes a Game object or a file path, parses the replay if necessary, and populates the correct information
+            in the Computer object.
+        get_player -> Player
+            Takes an identifier (string connect code or physical port number), returns a
+            Player object matching that identifier. Raises IdentifierError if the identifier is not found.
+        get_opponent -> Player
+            Takes an identifier (string connect code or physical port number), returns a Player object that does not
+            match that identifier, if that identifier matches one player in the game.
+    """
     replay: Game | None
     replay_version: Start.SlippiVersion | None
     queue: list[dict]
@@ -78,8 +115,7 @@ class ComputerBase:
     players: list[Player]
 
     def prime_replay(self, replay: PathLike | Game | str):
-        """Parses a replay and loads the relevant data into the combo computer. Call combo_compute(connect_code) to extract combos
-        from parsed replay"""
+        """Parses a replay and loads the relevant data into the Computer. Also accepts pre-parsed Game objects."""
         if isinstance(replay, (PathLike, str)):
             parsed_replay = Game(replay)
             self.replay_path = replay
@@ -169,24 +205,43 @@ class ComputerBase:
                     # TODO probably rip this out and just replace it with a log warning when done debugging
                     raise IdentifierError(f"No player matching given connect code {identifier}")
             case int() | Ports():
-                return self.players[identifier]
+                for player in self.players:
+                    if player.port == identifier:
+                        return player
+                else:
+                    raise IdentifierError(f"No player matching given port number {identifier}")
             case _:
                 raise IdentifierError(
-                    f"Invalid identifier type: {identifier} {type(identifier)}. get_player() accepts str, int, or Port"
+                    f"""Invalid identifier type for identifier: {identifier}.
+                    Got: {type(identifier)} Expected: str | int | Ports"""
                 )
 
+
     def get_opponent(self, identifier: str | int | Ports) -> Player:
+        opponent = None
+        valid_id = False
+
         match identifier:
             case str():
                 for player in self.players:
-                    if player.connect_code != identifier:
-                        return player
-                else:
-                    # TODO probably rip this out and just replace it with a log warning when done debugging
-                    raise ValueError(f"No player matching given connect code {identifier}")
+                    if player.connect_code == identifier:
+                        valid_id = True
+                    else:
+                        opponent = player
             case int() | Ports():
-                return self.players[identifier - 1]
+                for player in self.players:
+                    if player.port == identifier:
+                        valid_id = True
+                    else:
+                        opponent = player
             case _:
-                raise ValueError(
-                    f"Invalid identifier type: {identifier} {type(identifier)}. get_player() accepts str, int, or Port"
+                raise IdentifierError(
+                    f"""Invalid identifier type for identifier: {identifier}.
+                    Got: {type(identifier)} Expected: str | int | Ports"""
                 )
+
+        if valid_id:
+            return opponent
+        else:
+            raise IdentifierError(f"""Cannot find opponent for identifier {identifier}.
+                                  {identifier} is not present in game""")
