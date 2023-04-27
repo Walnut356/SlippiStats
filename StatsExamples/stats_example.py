45db49d72dabaf01a1d55ae5ade474e080c34b77
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-04-27 01:32:18 -0500

    repo cleaning for release

--- StatsExamples/stats_example.py
+++ StatsExamples/stats_example.py
@@ -1,156 +1,53 @@
+# import slippistats as slp
 from pathlib import Path
-import timeit
-import os, concurrent.futures, datetime
-
 import polars as pl
-from slippistats import *
 
-# c_code = "NUT#356"
-# file = Path(r"Modern Replays/ACTI#799 (Falcon) vs NUT#356 (Falco) on FoD - 12-15-22 02.06am .slp")
-# replay = Game(file)
-# stats = StatsComputer()
-# stats.prime_replay(replay)
-# stats.stats_compute("NUT#356")
+# replay_dir = Path(R"E:\Slippi Replays\Netplay")
+# connect_code = "NUT#356"
 
-
-def get_default_header(stats_computer: StatsComputer, connect_code: str) -> dict:
-
-    formatted_date = stats_computer.replay.metadata.date.replace(tzinfo=None)
-    # total number of frames, starting when the player has control, in seconds
-    formatted_time = datetime.timedelta(seconds=((stats_computer.replay.metadata.duration) / 60))
-
-    [player_port], opponent_port = stats_computer.generate_player_ports(connect_code)
-
-    header = {
-        "match_id": stats_computer.replay.start.match_id,
-        "date_time": formatted_date,
-        "duration": formatted_time,
-        "ranked": stats_computer.replay.start.is_ranked,
-        "win": stats_computer.is_winner(player_port),
-        "char": id.InGameCharacter(list(stats_computer.players[player_port].characters.keys())[0]).name,  #lmao
-        "opnt_Char": id.InGameCharacter(list(stats_computer.players[opponent_port].characters.keys())[0]).name
-        }
-
-    return header
+# if __name__ == "__main__":
+#     stats = slp.get_stats(replay_dir, connect_code)
+#     pass
+#     stats.wavedashes.write_parquet(R"StatsExamples\Output\wavedashes.parquet")
+#     stats.dashes.write_parquet(R"StatsExamples\Output\dashes.parquet")
+#     stats.techs.write_parquet(R"StatsExamples\Output\techs.parquet")
+#     stats.take_hits.write_parquet(R"StatsExamples\Output\take_hits.parquet")
+#     stats.l_cancels.write_parquet(R"StatsExamples\Output\l_cancels.parquet")
+#     pass
 
 
-def get_wavedash_data(replay, connect_code) -> dict:
-    stats = StatsComputer()
-    stats.prime_replay(replay)
-    stats.wavedash_compute(connect_code)
+def l_cancel_demo():
+    l_cancel = pl.read_parquet(Path(R"StatsExamples\Output\l_cancels.parquet"))
 
-    header = get_default_header(stats, connect_code)
-    wd_data = [header | wavedash.__dict__ for wavedash in stats.data.wavedashes]
-    return wd_data
+    percent = l_cancel.filter(pl.col("character") == "FALCO").groupby("l_cancel").agg(pl.count())
 
+    print(percent)
 
-def get_dash_data(replay, connect_code) -> dict:
-    stats = StatsComputer()
-    stats.prime_replay(replay)
-    stats.wavedash_compute(connect_code)
+    by_move = l_cancel.filter(pl.col("character") == "FALCO").groupby(pl.col("move")).agg([pl.col("l_cancel").mean()])
 
-    header = get_default_header(stats, connect_code)
-    dash_data = [header | dash.__dict__ for dash in stats.data.dashes]
-    return dash_data
+    print(by_move)
 
+    by_move_by_location = (
+        l_cancel.filter(pl.col("character") == "FALCO")
+        .groupby(pl.col("position"))
+        .agg([pl.col("l_cancel").mean(), pl.count()])
+    )
 
-def get_tech_data(replay, connect_code) -> dict:
-    stats = StatsComputer()
-    stats.prime_replay(replay)
-    stats.wavedash_compute(connect_code)
+    print(by_move_by_location)
 
-    header = get_default_header(stats, connect_code)
-    tech_data = [header | tech.__dict__ for tech in stats.data.tech]
-    return tech_data
+    by_opnt = (
+        l_cancel.filter(pl.col("character") == "FALCO")
+        .groupby(pl.col("opnt_character"))
+        .agg([pl.col("l_cancel").mean(), pl.count()])
+        .sort(pl.col("count"))
+    )
 
+    print(by_opnt)
 
-def get_take_hit_data(replay, connect_code) -> dict:
-    stats = StatsComputer()
-    stats.prime_replay(replay)
-    stats.wavedash_compute(connect_code)
+    by_stocks = (
+        l_cancel.filter(pl.col("character") == "FALCO")
+        .groupby(pl.col("stocks_remaining"))
+        .agg([pl.col("l_cancel").mean(), pl.count()])
+    )
 
-    header = get_default_header(stats, connect_code)
-    take_hit_data = [header | take_hit.__dict__ for take_hit in stats.data.take_hit]
-    return take_hit_data
-
-
-def get_l_cancel_data(replay, connect_code) -> dict:
-    stats = StatsComputer()
-    stats.prime_replay(replay)
-    stats.wavedash_compute(connect_code)
-
-    header = get_default_header(stats, connect_code)
-    l_cancel_data = [header | l_cancel.__dict__ for l_cancel in stats.data.l_cancel]
-    return l_cancel_data
-
-
-def get_general_data(replay, connect_code) -> dict:
-    stats = StatsComputer()
-    stats.prime_replay(replay)
-    stats.wavedash_compute(connect_code)
-
-    header = get_default_header(stats, connect_code)
-
-    #TODO aggregate data from other data outputs
-
-
-def stats_from_file(file, connect_code: str) -> pl.DataFrame:
-    """Accept file path and connect code, process combos, and return"""
-    print(file)
-    data_frame = pl.DataFrame(get_wavedash_data(file, connect_code))
-    try:
-        data_frame.sort("date_time")
-    except:
-        data_frame = None
-    return data_frame
-
-
-def multi_find_stats(dir_path, connect_code: str):
-    dfs = None
-    ind = 0
-    with os.scandir(dir_path) as thing:
-        #     for entry in thing:
-        #         df = stats_from_file(os.path.join(dir_path, entry.name), connect_code)
-        #         print("file processed")
-        #         if df is not None:
-        #             doodad = pl.read_parquet("wavedashdata.parquet")
-        #             doodad = pl.concat([doodad, df], how='vertical')
-        #             print(doodad)
-        #             doodad.write_parquet("wavedashdata.parquet")
-        #             print("file written")
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            futures = {
-                executor.submit(stats_from_file, os.path.join(dir_path, entry.name), connect_code) for number, entry in enumerate(thing)
-                }
-
-            for future in concurrent.futures.as_completed(futures):
-                print(f"processed {ind}")
-                ind += 1
-                if future.result() is not None:
-                    if dfs is None:
-                        dfs = future.result()
-                        print("creating initial")
-                    else:
-                        dfs = pl.concat([dfs, future.result()], how='vertical')
-                        print("concatting")
-    dfs.write_parquet("wavedashdata_temp.parquet")
-    print("file written")
-
-    return dfs
-
-    # print("start concat")
-    # doodad = pl.concat(dfs, how='vertical')
-    # print("start write")
-    # doodad.write_parquet("wavedashdata_temp.parquet")
-    print("Done")
-
-
-if __name__ == '__main__':
-    replay_dir = Path(input("Please enter the path to your directory of your replay files: "))
-    code_input = input("Please enter your connect code (TEST#123): ")
-
-    replay_dir = Path(r"C:\Users\ant_b\Documents\Coding Projects\starcraft calculator\sc2calc\py-slippi\Modern Replays")
-    code_input = "NUT#356"
-
-    print("Processing...")
-    multi_find_stats(replay_dir, code_input)
+    print(by_stocks)
