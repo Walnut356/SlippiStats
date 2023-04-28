6a970c7929fd1cbf2c5d8f3ae2a35cbb01734ba3
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-04-27 23:52:44 -0500

    updated init

--- slippistats/__init__.py
+++ slippistats/__init__.py
@@ -5,4 +5,4 @@ from .stats.stats_computer import StatsComputer, get_stats
 from .util import Port
 from .stats.computer import IdentifierError, PlayerCountError
 from .stats.common import get_playback_header
-import enums
+from .enums import *
